b18037f15405146400aef417a4e20ee5d628f061
cartoon-raccoon <shaughn.chan@gmail.com>
2021-01-30 15:11:32 +0800

    Updated pawn methods

--- src/pieces/pawn.rs
+++ src/pieces/pawn.rs
@@ -1,18 +1,17 @@
 use crate::pieces::Piece;
 use crate::moves::MoveSet;
+use crate::board::Board;
 
-const POINTS: u8 = 1;
+pub const POINTS: u8 = 1;
 
-pub struct Pawn {
-
-}
+pub struct Pawn;
 
 impl Piece for Pawn {
-    fn move_piece() {
+    fn move_piece(&self, board: &mut Board) {
 
     }
 
-    fn evaluate_moves() -> MoveSet {
+    fn evaluate_moves(&self, board: &mut Board) -> MoveSet {
         unimplemented!()
     }
 }
