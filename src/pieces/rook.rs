f1854e49fe685b1740f235501fb2c4fe1e5f44e6
cartoon-raccoon <shaughn.chan@gmail.com>
2021-01-30 21:12:59 +0800

    Added rook

---
+++ src/pieces/rook.rs
@@ -0,0 +1,18 @@
+use crate::pieces::Piece;
+use crate::moves::MoveSet;
+use crate::board::Board;
+
+pub const POINTS: u8 = 5;
+
+#[derive(Clone, Copy, Default)]
+pub struct Rook;
+
+impl Piece for Rook {
+    fn move_piece(&self, board: &mut Board) {
+
+    }
+
+    fn evaluate_moves(&self, board: &mut Board) -> MoveSet {
+        unimplemented!()
+    }
+}
