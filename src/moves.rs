08c0915c763dab1d2e09fc33eef8a2a53f605aea
cartoon-raccoon <shaughn.chan@gmail.com>
2021-01-31 12:56:22 +0800

    Added Traits and additional Move impls
    - Added MoveSet::from to construct a MoveSet from a generic IntoIterator that yields Option<T: MoveMarker>
    - Added MoveMarker trait for loose coupling in MoveSet::from
    - Added Move::construct to easily construct moves within MoveSet::from

--- src/moves.rs
+++ src/moves.rs
@@ -2,12 +2,33 @@ use core::ops::{Deref, DerefMut};
 use core::convert::AsRef;
 use std::vec::IntoIter;
 
+/// Allows a generic type to be marked as a Move.
+pub trait MoveMarker {
+    fn target(&self) -> (usize, usize);
+    fn origin(&self) -> (usize, usize);
+}
+
 #[derive(Clone)]
 pub struct MoveSet {
     moves: Vec<Move>
 }
 
 impl MoveSet {
+
+    /// Constructs a MoveSet from a type that implements IntoIterator
+    /// and its associated Item implements MoveMarker.
+    pub fn from<T: IntoIterator>(iter: T) -> Self
+    where T::Item: MoveMarker {
+        let mut moves = Vec::new();
+        
+        for item in iter.into_iter() {
+            moves.push(Move::construct(item.target(), item.origin()));
+        }
+
+        MoveSet {
+            moves: moves
+        }
+    }
     pub fn len(&self) -> usize {
         self.moves.len()
     }
@@ -98,8 +119,27 @@ impl Iterator for MoveSetIntoIter {
 
 #[derive(Copy, Clone, Debug)]
 pub struct Move {
-    pub target: (usize, usize),
-    pub origin: (usize, usize),
+    target: (usize, usize),
+    origin: (usize, usize),
+}
+
+impl Move {
+    pub(crate) fn construct(t: (usize, usize), o: (usize, usize)) -> Self {
+        Move {
+            target: t,
+            origin: o,
+        }
+    }
+}
+
+impl MoveMarker for Move {
+    fn target(&self) -> (usize, usize) {
+        self.target
+    }
+
+    fn origin(&self) -> (usize, usize) {
+        self.target
+    }
 }
 
 //todo: implement Display and Debug
